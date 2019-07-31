5c9a48c2a9ddb400e5ca4c44d27b2e69990831cd
Nitin Vohra <nitinvohra92@gmail.com>
2019-07-31 13:28:08 +0530

    books app

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const app = express()
 const Product =require('./db').Product
+const path = require('path')
+const hbs = require('hbs')
 
 
 app.set('view engine', 'hbs')
 app.set('views', __dirname + '/views')
+
+hbs.registerPartials(path.join(__dirname, '/partials' ))
 
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
