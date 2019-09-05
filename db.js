64f359de1ba4f9bfb26eba3d2392da07a6e54322
Nitin Vohra <nitinvohra92@gmail.com>
2019-09-05 20:08:57 +0530

    books app

--- db.js
+++ db.js
@@ -9,61 +9,61 @@ const db = new Sequelize({
 
 
 const User = db.define('users', {
-    id: {
-      type: Sequelize.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-      allowNULL:true  
-    },
-    username: {
-      type: Sequelize.STRING,
-      allowNull: false
-    },
-    password: {
-        type: Sequelize.STRING,
-        allowNULL:false
-    }
-  })
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNULL: true
+  },
+  username: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  password: {
+    type: Sequelize.STRING,
+    allowNULL: false
+  }
+})
 
-  const Product = db.define('products',{
-    id: {
-        type: Sequelize.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-        allowNULL:false
-      },
-      name: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      publishers: {
-         type: Sequelize.STRING,
-         allowNull:false
-      },
-      condition: {
-        type: Sequelize.STRING,
-        defaultValue: 'Good'
-      },
-      price: {
-            type: Sequelize.FLOAT,
-            allowNULL: false,
-            defaultValue: 0.0 
-     },
-     address: {
-         type: Sequelize.TEXT,
-         allowNULL: false
-     },
-     incart: {
-      type: Sequelize.INTEGER,
-      defaultValue: 0,
-      allowNULL:false
-    }
-  })
-  
-  db.sync()
-  .then(()=>console.log('database synced'))
-  .catch((err)=>console.log("error"))
+const Product = db.define('products', {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNULL: false
+  },
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  publishers: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  condition: {
+    type: Sequelize.STRING,
+    defaultValue: 'Good'
+  },
+  price: {
+    type: Sequelize.FLOAT,
+    allowNULL: false,
+    defaultValue: 0.0
+  },
+  address: {
+    type: Sequelize.TEXT,
+    allowNULL: false
+  },
+  incart: {
+    type: Sequelize.INTEGER,
+    defaultValue: 0,
+    allowNULL: false
+  }
+})
+
+db.sync()
+  .then(() => console.log('database synced'))
+  .catch((err) => console.log("error"))
 
 exports = module.exports = {
-    User, Product
+  User, Product
 }
